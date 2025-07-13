30fad371a90732c0bd1a8fd60cf499e7122262de
天子方辰 <tianzifangchen@gmail.com>
2025-07-13 14:20:42 -0400

    Fix: escape emoticons

--- bili-keyword-replacer.user.js
+++ bili-keyword-replacer.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/TZFC/Danmaku-replace
 // @downloadURL  https://raw.githubusercontent.com/TZFC/Danmaku-replace/main/bili-keyword-replacer.user.js
 // @updateURL    https://raw.githubusercontent.com/TZFC/Danmaku-replace/main/bili-keyword-replacer.user.js
-// @version      1.4
+// @version      2.0
 // @description  Replaces chosen substrings in outgoing Bilibili live-chat messages before they are sent
 // @author       TZFC
 // @match        https://live.bilibili.com/*
@@ -49,6 +49,10 @@
   function patchedBody(body) {
     if (!body) return body;
 
+    if (typeof body.has === 'function' && body.has('emoticonOptions')) {
+    return body;
+    }
+    
     const m = body.get('msg');
     if (m) body.set('msg', transformMsg(m));
     return body;
